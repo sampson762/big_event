1d6b58e971bfa1e70dbf0d52a10742b362f50ac2
baofeng <s1769301207@163.com>
2020-11-21 18:23:44 +0800

    完成了主页功能得开发

--- assets/js/baseapi.js
+++ assets/js/baseapi.js
@@ -2,4 +2,18 @@ $.ajaxPrefilter(function(opt){
     opt.url = "http://ajax.frontend.itheima.net" + opt.url
 
 
+    if (opt.url.indexOf('/my/') !== -1) {
+        opt.headers = {
+          Authorization: localStorage.getItem('token') || ''
+        }
+      }
+
+     opt.complete = function(res){
+        if(res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！"){
+            console.log('ok');
+            localStorage.removeItem('token')
+            location.href='/login.html'
+        }
+    }
+
 })
---
+++ assets/js/index.js
@@ -0,0 +1,60 @@
+
+
+$(function(){
+
+
+
+    userinfo();
+  
+   
+    var layer = layui.layer
+    $('#close_btn').on('click',function(){
+        layer.confirm('确定退出吗?', {icon: 3, title:'提示'}, function(index){
+            localStorage.removeItem('token')
+            location.href= '/login.html'
+            layer.close(index);
+          });
+
+
+    })
+
+})
+
+  function userinfo(){
+    
+    $.ajax({
+        method:"GET",
+        url: "/my/userinfo",
+        // headers:{
+        //     Authorization:localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            if(res.status !== 0 ) return layui.layer.msg('获取用户信息失败')
+            avater(res.data);
+        },
+    
+     
+    });
+
+
+  }
+
+  function avater(user) {
+
+    var name = user.username ||nickname 
+    
+    $('#welcome').html('欢迎&nbsp;&nbsp;'+name)
+
+    if(user.user_pic !== null) {
+          $('.text-avater').hide()
+          $('.layui-nav-img').attr('src',user.user_pic).show()
+
+    }else {
+        var first = name[0].toUpperCase()
+        $('.text-avater').html(first).show()
+        $('.layui-nav-img').hide()
+    }
+    // $('.text-avater').html('name[0].toUpperCase')     
+
+
+  }
